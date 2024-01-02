e36801aa21fff0b9759dee6ab930db63944ea66e
faradox <felix@okio.ai>
2024-01-02 11:47:10 +0100

    chore: prepare release 0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 if __name__ == "__main__":
     setup(
         name="nendo-plugin-quantize-core",
-        version="0.1.5",
+        version="0.2.0",
         description="Nendo core quantizer plugin",
         author="Okio Team <team@okio.ai>",
     )
--- src/nendo_plugin_quantize_core/__init__.py
+++ src/nendo_plugin_quantize_core/__init__.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from .plugin import CoreQuantizer
 
-__version__ = "0.1.5"
+__version__ = "0.2.0"
 
 __all__ = [
     "CoreQuantizer",
